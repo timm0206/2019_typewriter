0f7fbc9acd9f2d5b9b812e8d824c9c8d4845f0fa
timm0206 <timm0206@stud.kea.dk>
2019-02-11 13:26:41 +0100

    Initial start Text fremme på skærmen

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,47 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", Init);
+
+function Init() {}
+
+console.log("Init complete");
+
+const text = document.querySelector("#typewriter").textContent;
+
+console.log(text);
+
+/* 
+
+TODO:
+
+Declare counter and text
+
+
+
+Setup INIT
+
+    Function 1
+
+    Store/Read the text from the div
+
+    Remove text from div
+
+    Reset counter
+
+    Start loop
+
+
+        Function 2
+
+        Find text from 0 to counter
+
+        Display text part in HTML
+
+        Counter ++ (Display one character at a time)
+
+        If text.length > counter go to loop start
+
+        Add delay (400ms)
+
+
+*/
